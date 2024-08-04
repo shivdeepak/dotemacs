023eb21f90f18191dc688e5e4e51d29f2337b159
Shiv Deepak Muddada <shiv@aeroh.org>
2024-08-04 16:46:10 -0700

    refactor flycheck code for python

--- .emacs.d/flycheck.el
+++ .emacs.d/flycheck.el
@@ -7,19 +7,25 @@
 ;; flycheck
 (add-hook 'python-mode-hook 'flycheck-mode)
 
-(setq flycheck-python-flake8-executable (expand-file-name "flake8" conda-bin-dir))
+(setq flycheck-python-flake8-executable
+      (expand-file-name "flake8" conda-bin-dir))
 
-;; setup ~/.flake8 config file
 (with-eval-after-load 'flycheck
-  (setq-default flycheck-flake8rc (expand-file-name ".flake8" (getenv "HOME"))))
-
-;; disable python-mypy and python-pylint and only keep python-flake8
-(with-eval-after-load 'flycheck
-  (setq-default flycheck-disabled-checkers '(python-mypy python-pylint)))
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (flycheck-select-checker 'python-flake8)))
+  (lambda ()
+    (if (derived-mode-p 'python-mode)
+	(
+	 ;; setup ~/.flake8 config file
+	 (setq-default flycheck-flake8rc
+		       (expand-file-name ".flake8" (getenv "HOME")))
+	 ;; disable python-mypy and python-pylint and
+	 ;; only keep python-flake8
+	 (setq-default flycheck-disabled-checkers
+		       '(python-mypy python-pylint))
+	 ;; select python-flake8 checker
+	 (flycheck-select-checker 'python-flake8))
+      )
+    )
+  )
 
 (global-set-key (kbd "C-c f l") 'flycheck-list-errors)
 
