cf4c61061a56cd38a9aa879539656ba58d76f19f
Shiv Deepak Muddada <shivdeepak@gmail.com>
2024-08-12 09:27:37 -0700

    dont create *~ files in the current directory

--- .emacs.d/editing.el
+++ .emacs.d/editing.el
@@ -9,3 +9,9 @@
 (setq column-number-mode t)
 (setq fill-column 79)
 (global-display-fill-column-indicator-mode 1)
+
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
